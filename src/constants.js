d19d8ed6b66f8a192b8fd38dc043923593f80795
Jordan Eldredge <jordan@jordaneldredge.com>
2017-02-06 17:40:03 -0800

    Use a different auth url in prod than in dev

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,7 @@
+// TODO: Find an approach where dev values get removed by Uglify's dead code detection.
+const devProd = (dev, prod) => (
+    process.env.NODE_ENV === "development" ? dev : prod
+)
+export const AUTH_REDIRECT_URL = devProd("http://localhost:3000/auth/","https://md.jordaneldredge.com/auth/");
+
 export const TITLE_DATE_FORMAT = 'dddd, MMMM Do, YYYY';
