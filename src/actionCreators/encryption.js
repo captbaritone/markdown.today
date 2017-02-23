e7a39ccbe976d365acd0bd79e186d1eb2a5ced0a
Jordan Eldredge <jordan@jordaneldredge.com>
2017-02-22 21:42:45 -0800

    Add more TODOs

--- src/actionCreators/encryption.js
+++ src/actionCreators/encryption.js
@@ -2,10 +2,14 @@ import { SET_ENCRYPTION_PASSWORD } from "../actionTypes";
 import { uploadToDropbox } from "./dropbox";
 import { addNotification } from "./";
 
+// Set the encryption password we want to use.
 export const setEncryptionPassword = password => ({
   type: SET_ENCRYPTION_PASSWORD,
   password
 });
+
+// Reencrypt the journal with a new password.
+// TODO: Rename
 export const updateEncryptionPassword = password => {
   return dispatch => {
     dispatch(setEncryptionPassword(password));
