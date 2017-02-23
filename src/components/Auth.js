552a0ff648af26ec23415abb24de858bb229e92e
Jordan Eldredge <jordan@jordaneldredge.com>
2017-02-22 21:43:08 -0800

    Split out logic to improve readability

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -4,9 +4,12 @@ import { push } from "react-router-redux";
 import { parse } from "qs";
 import { downloadJournal } from "../actionCreators";
 
+const getHash = () => this.props.location.hash.slice(1);
+const getAccessTokenFromHash = () => parse(getHash()).access_token;
+
 class Auth extends React.Component {
   componentWillMount() {
-    const accessToken = parse(this.props.location.hash.slice(1)).access_token;
+    const accessToken = getAccessTokenFromHash();
     // TODO: Convert to action creator
     this.props.dispatch({ type: "SET_AUTH_TOKEN", token: accessToken });
     this.props.dispatch(downloadJournal());
