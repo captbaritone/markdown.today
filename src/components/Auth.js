54625db538a3523b36915db81f4c26fc2148a849
Jordan Eldredge <jordan@jordaneldredge.com>
2017-02-24 19:09:35 -0800

    Avoid double downloading jounal

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,14 +2,12 @@ import React from "react";
 import { connect } from "react-redux";
 import { push } from "react-router-redux";
 import { parse } from "qs";
-import { downloadJournal } from "../actionCreators";
 
 class Auth extends React.Component {
   componentWillMount() {
     const accessToken = parse(this.props.location.hash.slice(1)).access_token;
     // TODO: Convert to action creator
     this.props.dispatch({ type: "SET_AUTH_TOKEN", token: accessToken });
-    this.props.dispatch(downloadJournal());
     // TODO: Get desired URL from URL or some such thing
     this.props.dispatch(push("/"));
   }
