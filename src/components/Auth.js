a3d20ab2ea490496a014783d0f0b926aa0189b36
Jordan Eldredge <jordan@jordaneldredge.com>
2017-02-27 08:33:29 -0800

    Document pseudo 301

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -7,6 +7,10 @@ import { setAuthToken } from "../actionCreators";
 
 class Auth extends React.Component {
   componentWillMount() {
+    // This is essentially a 301 redirect. Dropbox will redirect to a URL that
+    // loads this route. This route will never render, it simply does some work
+    // and then sends you to a different route. Kinda gross, but I don't really
+    // see a much better solution.
     const accessToken = parse(this.props.location.hash.slice(1)).access_token;
     this.props.dispatch(setAuthToken(accessToken));
     // TODO: Get desired URL from URL or some such thing
