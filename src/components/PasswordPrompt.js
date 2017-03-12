e046496c327f0e114e478899b3962308d06684bb
Jordan Eldredge <jordan@jordaneldredge.com>
2017-03-11 21:12:24 -0800

    Require submission to decrypt.

--- src/components/PasswordPrompt.js
+++ src/components/PasswordPrompt.js
@@ -12,22 +12,37 @@ import {
 } from "../actionCreators";
 import { needsEncryptionPassword, getEncryptedBlob } from "../accessors";
 
-class PasswordPrompt extends React.Component {
+export class PasswordPrompt extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { password: "" };
+    this.state = { password: "", isValid: false };
     this.updatePassword = this.updatePassword.bind(this);
+    this.checkValidity = this.checkValidity.bind(this);
+    this.decrypt = this.decrypt.bind(this);
+  }
+
+  checkValidity() {
+    let isValid = true;
+    try {
+      decrypt(this.state.password, this.props.encryptedBlob);
+    } catch (e) {
+      console.log("fail");
+      isValid = false;
+    }
+    return isValid;
   }
 
   updatePassword(e) {
     const password = e.target.value;
     this.setState({ password });
-    try {
-      decrypt(password, this.props.encryptedBlob);
-    } catch (e) {
+  }
+
+  decrypt(e) {
+    e.preventDefault();
+    if (!this.checkValidity()) {
       return;
     }
-    this.props.setEncryptionPassword(password);
+    this.props.setEncryptionPassword(this.state.password);
     this.props.attemptToDecryptJournal();
   }
 
@@ -47,21 +62,38 @@ class PasswordPrompt extends React.Component {
               onTouchTap={this.props.logout}
             />
           ),
-          <FlatButton disabled={true} label="Decrypt" primary={true} />
+          (
+            <FlatButton
+              disabled={!this.checkValidity()}
+              onTouchTap={this.decrypt}
+              label="Decrypt"
+              primary={true}
+            />
+          )
         ]}
       >
-        <TextField
-          hintText="Encryption Password"
-          floatingLabelText="Password"
-          type="password"
-          onChange={this.updatePassword}
-          value={this.state.password}
-          autoFocus
-        />
+        <form onSubmit={this.decrypt}>
+          <TextField
+            hintText="Encryption Password"
+            floatingLabelText="Password"
+            type="password"
+            onChange={this.updatePassword}
+            value={this.state.password}
+            autoFocus
+          />
+        </form>
       </Dialog>
     );
   }
 }
+
+PasswordPrompt.propTypes = {
+  open: React.PropTypes.bool.isRequired,
+  encryptedBlob: React.PropTypes.string,
+  attemptToDecryptJournal: React.PropTypes.func.isRequired,
+  setEncryptionPassword: React.PropTypes.func.isRequired,
+  logout: React.PropTypes.func.isRequired
+};
 
 const mapStateToProps = state => ({
   open: needsEncryptionPassword(state),
